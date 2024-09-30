59f588f90d6e9ef87793c9017cb225a5c1f83cfd
Ioannis Jone Magalhaes <ij21317@essex.ac.uk>
2024-09-30 14:17:04 +0100

    created playerAccounts and player models

--- Django/RPS_Project/RPS/admin.py
+++ Django/RPS_Project/RPS/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import playerAccount,player
 
+admin.site.register(playerAccount)
+admin.site.register(player)
 # Register your models here.
--- Django/RPS_Project/RPS/models.py
+++ Django/RPS_Project/RPS/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class playerAccount(models.Model):
+    userName = models.CharField(max_length=127)
+    email = models.EmailField(max_length=255)
+    dateJoined = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+          return self.userName
+
+class player(models.Model):
+        playerName = models.CharField(max_length=255)
+        wins = models.IntegerField()
+        losses = models.IntegerField()
+    
+        def __str__(self):
+            return self.playerName
+
+
 # Create your models here.
