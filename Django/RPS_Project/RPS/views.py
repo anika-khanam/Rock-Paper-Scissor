cf33984b3c0c2d6713c0f72ab3cfa4744e8f0e16
Tom Moyse <thomashmoyse@gmail.com>
2024-09-30 15:53:42 +0100

    minor changes

--- Django/RPS_Project/RPS/views.py
+++ Django/RPS_Project/RPS/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from .models import PlayerAccount,Player
 from .serializers import PlayerAccountSerializer,PlayerSerializer
 from django.shortcuts import render
+import random
 
 class PlayerAccountList(generics.ListCreateAPIView):
     serializer_class = PlayerAccountSerializer
@@ -27,4 +28,27 @@ class PlayerList(generics.ListCreateAPIView):
 # Create your views here.
 class GameRound(APIView):
     def post(self, request):
+        print(self.pretty_request(request))
         return Response(random.choice(["rock","paper","scissors"]))
+
+    def pretty_request(self, request):
+        headers = ''
+        for header, value in request.META.items():
+            if not header.startswith('HTTP'):
+                continue
+            header = '-'.join([h.capitalize() for h in header[5:].lower().split('_')])
+            headers += '{}: {}\n'.format(header, value)
+
+        return (
+            '{method} HTTP/1.1\n'
+            'Content-Length: {content_length}\n'
+            'Content-Type: {content_type}\n'
+            '{headers}\n\n'
+            '{body}'
+        ).format(
+            method=request.method,
+            content_length=request.META['CONTENT_LENGTH'],
+            content_type=request.META['CONTENT_TYPE'],
+            headers=headers,
+            body=request.body,
+        )
--- rock-paper-scissors/src/components/game/Game.js
+++ rock-paper-scissors/src/components/game/Game.js
@@ -5,13 +5,14 @@ import axios from 'axios'
 function GameComponent() {
     const [loading, setLoading] = useState(false);
     const [status, setStatus] = useState('');
-    const apiURL = 'http://127.0.0.1:8000/endpoints/gameround/';
+    const apiURL = 'http://127.0.0.1:8000/gameround/';
 
     const submitSelection = (choice) => {
+        console.log(choice, typeof(choice))
         const axPost = async () => {
             setLoading(true);
             try{
-                const resp = await axios.post(apiURL, choice)
+                const resp = await axios.post(apiURL, choice, {headers: {"Content-Type": "text/plain"}})
                 console.log(resp);
                 console.log(resp.data);
                 // Expect response with opponent choice
